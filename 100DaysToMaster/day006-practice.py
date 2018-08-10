97574c69172bffbcdb019ded7febe86ab318d913
Stuart <stuart.north@gmail.com>
2018-08-10 15:29:37 +0100

    Upload existing files on Git initialised

---
+++ 100DaysToMaster/day006-practice.py
@@ -0,0 +1,13 @@
+# find all numbers between 2000 and 3200, divisible by 7 but NOT multiple of 5
+# solution should be comma separated on a single line
+
+# compute the factorial of a given number, result printed in csv single line
+# factorial is the integer product of a number and all the numbers below it
+# e.g. 4! is 4 * 3 * 2 * 1
+
+# given integral number n, create a dictionary with key n, and value is the square of n
+# integral is a number assigned to a function
+
+# for a given input of comma separated numbers, store each number in a list element
+# convert the list into a tuple, then print the list and the tuple
+# note the different way they are expressed
