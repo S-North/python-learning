51e403a713a3a140aa4221696f054c4f526a6175
S-North <stuart.north@gmail.com>
2019-05-20 11:35:54 +0100

    Update reorder-video-folders.py

--- My Projects/reorder-video-folders.py
+++ My Projects/reorder-video-folders.py
@@ -3,33 +3,68 @@
 # multiple files of the same name, with different extensions should be organised into single folders e.g. video, project, metadata files
 
 import os
+# s = enter the full path to the root directory containing your unsorted files. no training slash e.g. /path/to/your/folder
+# d = enter the full path to the root directory where you want your sorted files. This can be the same as s. no training slash e.g. /path/to/your/folder
+# extensions = only extensions listed in this variable will be sorted.
+extensions = ["mkv","mp4","avi","srt"]
+s = "/home/stuart/python-rename-folder/src-folder"
+d = "/home/stuart/python-rename-folder/dst-folder"
 
 # get the list of files and folders on the given path
 # this should be improved to deal with files with more than one period i.e. file extension checking
+for fn in os.listdir(s):
+	fullpath = str(s + "/" + fn)
+	filename = fn.split(".")
+	print(filename,len(filename))
 
-path = '/media/fileserver/films/'
-for fn in os.listdir(path):
-	filename, extension = os.path.splitext(fn)
-#	print("filename & extension is ",filename, " ", extension)
+	if os.path.isdir(fullpath) == True:
+		print("This is a directory")
+		print("")
 
-
-
-	src = path+fn
-	dst = path+filename+"/"+fn
-	folder = path+filename
-
-	if not os.path.isdir(os.path.join(folder)):
-		continue
-#		print("")
-#		print("source is ",src)
-#		print("create folder at ",folder)
-#		print("and move file to ",dst)
-#		os.makedirs(folder)
-#		os.rename(src,dst)
+	elif filename[-1] in (extensions) and len(filename) > 1:
+		ext = filename.pop()
+		name = " ".join(filename)
+		print('the extension is: ', ext)
+		print('the filename is: ', " ".join(filename))
+		print("")
 	else:
-		continue
-#		print("")
-#		print("source is ",src)
-#		print("folder already exists at ",folder)
-#		print("moving file to folder")
-#		os.rename(src,dst)
+		print("This file isn't an allowed type")
+		print("")
+
+
+#	if filename[-1] in ('mkv','mp4','avi'):
+#		print('file has a video file extension')
+#	else:
+#		print("file doesn't have a video file extension")
+#
+#	if len(filename) == 2:
+#		print('filename has 1 split on a period')
+#	elif len(filename) > 2:
+#		print('filename has more than 1 splits on a period')
+#	else:
+#		print('filename has NO splits on a period')
+
+#	print('')
+
+
+
+
+
+
+#	filename, extension = os.path.splitext(fn)
+#	print("filename & extension is ",filename, " ", extension)
+#
+#	src = path+"/"+fn
+#	print("source is ",src)
+#
+#	dst = path+"/"+filename+"/"+fn
+#
+#	folder = path+"/"+filename
+#	print("destination is ",dst)
+	#if not os.path.isdir(os.path.join(folder)):
+	#	print("no folder at ",folder)
+	#	os.makedirs(folder)
+	#	os.rename(src,dst)
+	#else:
+	#	print("folder exists at ",folder)
+	#	os.rename(src,dst)
