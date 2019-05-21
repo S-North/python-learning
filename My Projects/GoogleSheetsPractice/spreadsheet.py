ddee297e9ad11cca5cfb0ecefbe1b77c6fb8a406
S-North <stuart.north@gmail.com>
2019-05-21 11:51:19 +0100

    Google sheets api tested and working. Needed to ensure pip install gspread was installed using pip3

--- My Projects/GoogleSheetsPractice/spreadsheet.py
+++ My Projects/GoogleSheetsPractice/spreadsheet.py
@@ -1,8 +1,8 @@
-import gspread pprint
-from oath2client.service_account import ServiceAcccountCredentials
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
 
-scope = ['https://spreadsheets.google.com/feeds']
-creds = ServiceAcccountCredentials.from_json_keyfile_name('client_secret.json', scope)
+scope = ["https://spreadsheets.google.com/feeds",'https://www.googleapis.com/auth/spreadsheets',"https://www.googleapis.com/auth/drive.file","https://www.googleapis.com/auth/drive"]
+creds = ServiceAccountCredentials.from_json_keyfile_name("client_secret.json", scope)
 client = gspread.authorize(creds)
 
 sheet = client.open('Python test data - cleaning').sheet1
