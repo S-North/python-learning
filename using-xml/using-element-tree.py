7a2d3dae378ae2da6c8c88494131cfdc3f3e4cc0
s-north <stuart.north@gmail.com>
2021-03-15 14:15:43 +0000

    Updated files re learning xml

---
+++ using-xml/using-element-tree.py
@@ -0,0 +1,73 @@
+# from https://stackabuse.com/reading-and-writing-xml-files-in-python/
+import xml.etree.ElementTree as ET
+tree = ET.parse('compendiums/items.xml')
+root = tree.getroot()
+
+# one specific item attribute
+print('Item #2 attribute:')
+print("print(root[0][1].attrib)")
+print(root[0][1].attrib) # returns the attribute as a dictionary
+print(root[0][1].attrib['name']) # so we can get the value like this
+
+# all item attributes
+print('\nAll attributes:')
+print("""
+for elem in root:
+    for subelem in elem:
+        print(subelem.attrib)
+""")
+for elem in root:
+    for subelem in elem:
+        print(subelem.attrib)
+
+# one specific item's data
+print('\nItem #2 data:')
+print("print(root[0][1].text)")
+print(root[0][1].text)
+
+# all items data
+print('\nAll item data:')
+print("""
+for elem in root:
+    for subelem in elem:
+        print(subelem.text)
+""")
+for elem in root:
+    for subelem in elem:
+        print(subelem.text)
+
+# finding stuff
+print("\nfinding stuff")
+for elem in root:
+    print(elem.find('item').get('name'))
+
+## with the monster data
+## .....................
+print("\n"*2 + "."*70)
+compendium = ET.parse('compendiums/test-monsters.xml')
+root = compendium.getroot()
+
+print(f"Print the value of the 'version' attribute on the root element: {root.attrib['version']}")
+print(f"Print the value of the 'version' attribute on the first child: {root[0].attrib['version']}")
+print(f"Print the text of the first child pf the first child: {root[0][0].text}")
+
+print("\nfinding stuff")
+for elem in root:
+    print(elem.find('name').text)
+
+print("\nFinding actions")
+monster = root[0]
+for elem in monster.findall('action'):
+    print(elem.tag)
+    print(elem.find('name').text)
+    print(elem.find('text').text)
+    print(elem.find('attack').text)
+
+# Time to do some deeper reading https://docs.python.org/3/library/xml.etree.elementtree.html
+
+print(monster.tag)
+for child in monster:
+    print(child.tag)
+
+for elem in monster.findall(".='Blue Mage'"):
+    print(elem.text)
--- using-xml/using-xml.py
+++ using-xml/using-xml.py
@@ -1,7 +1,7 @@
 from xml.dom.minidom import parse
 import xml.dom.minidom
 
-DOMTree = xml.dom.minidom.parse("movies.xml")
+DOMTree = xml.dom.minidom.parse("compendiums/movies.xml")
 collection = DOMTree.documentElement
 if collection.hasAttribute("shelf"):
    print ("Root element : %s" % collection.getAttribute("shelf"))
@@ -21,3 +21,75 @@ for movie in movies:
    print ("Rating: %s" % rating.childNodes[0].data)
    description = movie.getElementsByTagName('description')[0]
    print ("Description: %s" % description.childNodes[0].data)
+
+print("\n"*3)
+
+DOMTree = xml.dom.minidom.parse("compendiums/Archmage")
+collection = DOMTree.documentElement
+if collection.hasAttribute("version"):
+   print ("Version is : %s" % collection.getAttribute("version"))
+
+monsters = collection.getElementsByTagName("monster")
+# actions = monsters.getElementsByTagName("action")
+
+for monster in monsters:
+   print ("*****Monster*****")
+   if monster.hasAttribute("name"):
+      print ("Name: %s" % monster.getAttribute("name"))
+
+   type = monster.getElementsByTagName('type')[0]
+   print ("Type: %s" % type.childNodes[0].data)
+   hp = monster.getElementsByTagName('hp')[0]
+   print ("HP: %s" % hp.childNodes[0].data)
+   ac = monster.getElementsByTagName('ac')[0]
+   print ("AC: %s" % ac.childNodes[0].data)
+   alignment = monster.getElementsByTagName('alignment')[0]
+   print ("Alignment: %s" % alignment.childNodes[0].data)
+
+   action = monster.getElementsByTagName('action')[0]
+   print ("Action: %s" % action.childNodes[0].data)
+
+# parse an xml file by name
+mydoc = xml.dom.minidom.parse('compendiums/items.xml')
+
+items = mydoc.getElementsByTagName('item')
+
+# one specific item attribute
+print('Item #2 attribute:')
+print(items[1].attributes['name'].value)
+
+# all item attributes
+print('\nAll attributes:')
+for elem in items:
+    print(elem.attributes['name'].value)
+
+# one specific item's data
+print('\nItem #2 data:')
+print(items[1].firstChild.data)
+print(items[1].childNodes[0].data)
+
+# all items data
+print('\nAll item data:')
+for elem in items:
+    print(elem.firstChild.data)
+
+print("\n"*2)
+# try this out on the archmage xml
+compendium = xml.dom.minidom.parse("compendiums/Archmage")
+monsters = compendium.getElementsByTagName('monster')
+# one specific item attribute
+print(f"Monster 1 version: {monsters[0].attributes['version'].value}")
+
+# all item attributes
+print("All version attributes")
+for elem in monsters:
+    print(elem.attributes['version'].value)
+
+# one specific item's data
+print(monsters[0].firstChild.data)
+print(monsters[0].childNodes[0].data)
+print(monsters[0].childNodes[1].data)
+print(monsters[0].childNodes[2].data)
+print(monsters[0].childNodes[3].data)
+print(monsters[0].childNodes[4].data)
+print(monsters[0].childNodes[5].data)
